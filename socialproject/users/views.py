da86d91c8891d62db8e057a091e9f3256f83e301
Haikal Fikri <haikaljr1245@gmail.com>
2023-10-03 16:06:12 +0700

    password functionality

--- socialproject/users/views.py
+++ socialproject/users/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
 from users.forms import LoginForm 
@@ -15,10 +16,15 @@ def user_login(request):
             
             if user is not None:
                 login(request, user)
-                return HttpResponse("You successfully login")
+                return redirect("index")
             else:
                 return HttpResponse("Invalid login ")
     else:
         form = LoginForm()        
     
     return render(request, "users/login.html", {'form': form})
+
+
+@login_required
+def index(request):
+    return render(request, "users/index.html")
