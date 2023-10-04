b654e07aac609db34b8cab73d68461b3956209ca
Haikal Fikri <haikaljr1245@gmail.com>
2023-10-04 21:33:22 +0700

    make functionality for edit user and user profile

--- socialproject/users/urls.py
+++ socialproject/users/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('register/', views.user_register, name="register"),
     path('login/', views.user_login, name="login"),
     path("logout/", auth_views.LogoutView.as_view(template_name="users/logout.html"), name="logout"),
     
@@ -16,5 +17,8 @@ urlpatterns = [
     path("password_reset/", auth_views.PasswordResetView.as_view(template_name="users/password_reset_form.html"), name="password_reset"),
     path("password_reset/done/", auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name="password_reset_done"),
     path("reset/<uidb64>/<token>", auth_views.PasswordResetConfirmView.as_view(template_name="users/password_reset_confirm.html"), name="password_reset_confirm"),
-    path("reset/done/", auth_views.PasswordResetCompleteView.as_view(template_name="users/password_reset_complete.html"), name="password_reset_complete")
+    path("reset/done/", auth_views.PasswordResetCompleteView.as_view(template_name="users/password_reset_complete.html"), name="password_reset_complete"),
+
+    # edit user data and user profile
+    path('edit/', views.edit, name="edit"),
 ]
--- socialproject/users/views.py
+++ socialproject/users/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from .models import Profile
 
-from users.forms import LoginForm 
+from django.contrib import messages
+
+from .forms import UserEditForm, ProfileEditForm
+from users.forms import LoginForm, RegisterForm 
 
 # Create your views here.
 def user_login(request):
@@ -25,6 +29,42 @@ def user_login(request):
     return render(request, "users/login.html", {'form': form})
 
 
+def user_register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        
+        if form.is_valid():
+            new_user = form.save(commit=False)
+            new_user.set_password(form.cleaned_data['password'])
+            new_user.save()
+            Profile.objects.create(user=new_user)
+            return render(request, 'users/register_done.html')
+    else:
+        form = RegisterForm()
+        
+    return render(request, 'users/register.html', {'form': form})
+            
+    
+
 @login_required
 def index(request):
     return render(request, "users/index.html")
+
+
+# edit user data and user profile
+@login_required
+def edit(request):
+    if request.method == "POST":
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile, data=request.POST, files=request.FILES)
+        
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, "profile update successfull")
+            return redirect('index')
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+        
+    return render(request, "users/edit.html", {'user_form': user_form, 'profile_form': profile_form})
