1e8b5e5ae03116d7c8a8208b49f90f79253d48ab
Haikal Fikri <haikaljr1245@gmail.com>
2023-10-16 21:32:53 +0700

    make like functionality

--- socialproject/post/urls.py
+++ socialproject/post/urls.py
@@ -3,4 +3,6 @@ from post import views
 
 urlpatterns = [
     path("create_post/", views.create_post, name="create_post"),
+    path("feed/", views.feed, name="feed"),
+    path('like/', views.like_post, name="like"),
 ]
--- socialproject/post/views.py
+++ socialproject/post/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import PostCreateForm
+from .models import Post
+from users.models import Profile
+from django.shortcuts import get_object_or_404
 
 
 # Create your views here.
@@ -16,6 +19,22 @@ def create_post(request):
             return redirect("index")
     else:
         form = PostCreateForm(data=request.GET)
-        
     
     return render(request, 'posts/create.html', {'form': form})
+
+
+def feed(request):
+    posts = Post.objects.all()
+    logged_user = request.user
+    return render(request, "posts/feed.html", {'posts': posts, 'logged_user':logged_user})
+
+
+def like_post(request):
+    post_id = request.POST.get('post_id')
+    post = get_object_or_404(Post, id=post_id)
+    if post.liked_by.filter(id=request.user.id).exists():
+        post.liked_by.remove(request.user)
+    else:
+        post.liked_by.add(request.user)
+    
+    return redirect('feed')
